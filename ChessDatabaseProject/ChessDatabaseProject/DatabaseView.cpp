dc7db036ea70802c1006c8d79b6b200022caf8fe
SergeRichard <serge.richard@rogers.com>
2017-07-08 12:02:00 -0300

    Piece on board is now updated when player makes move.

--- ChessDatabaseProject/ChessDatabaseProject/DatabaseView.cpp
+++ ChessDatabaseProject/ChessDatabaseProject/DatabaseView.cpp
@@ -16,7 +16,8 @@ void DatabaseView::DisplayBoard(const std::vector<std::string>& candidateMoves, 
 {
 	ClearScreen();
 
-	//ProcessPlayerMoves()
+	ResetBoard();
+	ProcessMoves(movesPlayed);
 
 	std::cout << m_board;
 	std::cout << std::endl;
@@ -26,9 +27,118 @@ void DatabaseView::DisplayBoard(const std::vector<std::string>& candidateMoves, 
 	}
 	std::cout << "White to move: ";
 }
+void DatabaseView::ProcessMoves(const std::vector<std::string>& movesPlayed) {
+	
+	std::string startMove;
+	std::string destMove;
+
+	for (auto mp : movesPlayed) {
+		std::string delimiters = "-x";
+		std::size_t start = mp.find_first_of(delimiters);
+
+		startMove = mp.substr(0, start);
+		destMove = mp.substr(start + 1);
+
+		int startCol = ConvertToArrayCol(startMove[0]);
+		int startRow = ConvertToArrayRow(startMove[1]);
+		int destCol = ConvertToArrayCol(destMove[0]);
+		int destRow = ConvertToArrayRow(destMove[1]);
+
+		ProcessMove(startCol, startRow, destCol, destRow);
+
+		int temp = 0;
+	}
+}
+void DatabaseView::ProcessMove(int startCol, int startRow, int destCol, int destRow) {
+	m_board[destRow][destCol] = m_board[startRow][startCol];
+	m_board[startRow][startCol] = "--";
+
+}
+int DatabaseView::ConvertToArrayCol(char colLetter) {
+	switch (colLetter)
+	{
+	case 'a':
+		return 0;
+		break;
+	case 'b':
+		return 1;
+		break;
+	case 'c':
+		return 2;
+		break;
+	case 'd':
+		return 3;
+		break;
+	case 'e':
+		return 4;
+		break;
+	case 'f':
+		return 5;
+		break;
+	case 'g':
+		return 6;
+		break;
+	case 'h':
+		return 7;
+		break;
+	default:
+		throw std::invalid_argument("Should only contain letters from a to h");
+		break;
+	}
+	return -1;
+}
+int DatabaseView::ConvertToArrayRow(char rowLetter) {
+	switch (rowLetter)
+	{
+	case '1':
+		return 7;
+		break;
+	case '2':
+		return 6;
+		break;
+	case '3':
+		return 5;
+		break;
+	case '4':
+		return 4;
+		break;
+	case '5':
+		return 3;
+		break;
+	case '6':
+		return 2;
+		break;
+	case '7':
+		return 1;
+		break;
+	case '8':
+		return 0;
+	default:
+		throw std::invalid_argument("Should only contain letters from 1 to 8");
+	}
+	return -1;
+}
 void DatabaseView::ClearScreen()
 {
 	std::cout << std::string(100, '\n');
+}
+void DatabaseView::ResetBoard() {
+	std::string boardTemp[8][8] =
+	{
+		{ "BR","BN","BB","BQ","BK","BB","BN","BR" },
+		{ "BP","BP","BP","BP","BP","BP","BP","BP" },
+		{ "--","--","--","--","--","--","--","--" },
+		{ "--","--","--","--","--","--","--","--" },
+		{ "--","--","--","--","--","--","--","--" },
+		{ "--","--","--","--","--","--","--","--" },
+		{ "WP","WP","WP","WP","WP","WP","WP","WP" },
+		{ "WR","WN","WB","WQ","WK","WB","WN","WR" }
+	};
+	for (int row = 0; row < 8; row++) {
+		for (int col = 0; col < 8; col++) {
+			m_board[row][col] = boardTemp[row][col];
+		}
+	}
 }
 std::ostream & operator<<(std::ostream & stream, const std::string board[8][8])
 {
--- ChessDatabaseProject/ChessDatabaseProject/DatabaseView.h
+++ ChessDatabaseProject/ChessDatabaseProject/DatabaseView.h
@@ -16,6 +16,14 @@ public:
 
 	void ClearScreen();
 
+	void ProcessMoves(const std::vector<std::string>& movesPlayed);
+
+	void ResetBoard();
+	int ConvertToArrayCol(char colLetter);
+	int ConvertToArrayRow(char rowLetter);
+
+	void ProcessMove(int startCol, int startRow, int destCol, int destRow);
+
 private:
 	std::string m_board[8][8] = 
 	{
@@ -26,7 +34,7 @@ private:
 		{ "--","--","--","--","--","--","--","--" },
 		{ "--","--","--","--","--","--","--","--" },
 		{ "WP","WP","WP","WP","WP","WP","WP","WP" },
-		{ "WR","WR","WR","WR","WR","WR","WR","WR" }
+		{ "WR","WN","WB","WQ","WK","WB","WN","WR" }
 	};
 };
 
